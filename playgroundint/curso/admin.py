01d9e4951e86f5ef46c79fef32d83075c9c53354
Inés Sabaidini <sabaidinii@gmail.com>
2022-11-26 19:49:46 -0300

    Fin Segunda Clase

--- playgroundint/curso/admin.py
+++ playgroundint/curso/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from curso.models import *
 
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Profesor)
+admin.site.register(Estudiante)
+admin.site.register(Entregable)
